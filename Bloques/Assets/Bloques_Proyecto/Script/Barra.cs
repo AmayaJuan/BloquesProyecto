02f161feffd6223a915522b3c535edbe989587ea
AmayaJuan <jpva360@gmail.com>
2017-07-26 16:37:26 -0500

    Tutorial 02
    Movimiento la barra

---
+++ Bloques/Assets/Bloques_Proyecto/Script/Barra.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barra : MonoBehaviour
+{
+    public float velocidad = .4f;
+
+    Vector3 posIncial;
+	
+	void Start ()
+    {
+        posIncial = transform.position;
+	}
+	
+	void Update ()
+    {
+        float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
+        float posX = transform.position.x + (tecladoHorizontal * velocidad * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(posX, -8, 8), transform.position.y, transform.position.z);
+	}
+
+    public void ResetInicial()
+    {
+        transform.position = posIncial;
+    }
+}
