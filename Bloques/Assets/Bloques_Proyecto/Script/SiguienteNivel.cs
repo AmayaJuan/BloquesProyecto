5dcdd0ae7b105272ae85d399e3d0206d6824ae83
AmayaJuan <jpva360@gmail.com>
2017-07-27 18:48:05 -0500

    Tutorial 10
    Siguiente Nivel

---
+++ Bloques/Assets/Bloques_Proyecto/Script/SiguienteNivel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SiguienteNivel : MonoBehaviour
+{
+    public string nivelACargar;
+    public float retraso;
+
+    [ContextMenu("Activar Cargar")]
+    public void ActivarCargar()
+    {
+        Invoke("CargarNivel", retraso);
+    }
+
+    void CargarNivel()
+    {
+        SceneManager.LoadScene(nivelACargar);
+    }
+
+    public bool EsUltimoNivel()
+    {
+        return nivelACargar == "Portada";
+    }
+}
